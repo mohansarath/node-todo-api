281bcb716ab6f70e054490e5ba4ef6e04e4dc5ad
sarath <sarath.sct@gmail.com>
2018-04-09 12:43:04 +0530

    ADD PATCH /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,29 +1,6 @@
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./../server/db/mongoose');
 const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
 
-var id = '5ac73ad293ee512af37e934311';
-
-if (!ObjectID.isValid(id)){
-    console.log('ID not valid');
-}
-
-// Todo.find({
-//     _id: id
-// }).then((todos) => {
-//     console.log('Todos', todos);
-// });
-
-// Todo.findOne({
-//     _id: id
-// }).then((todo) => {
-//     console.log('Todo', todo);
-// });
-
-Todo.findById(id).then((todo) => {
-    if(!todo) {
-        return console.log('Id not found');
-    }
-    console.log('Todo by id', todo);
-}).catch((e) => console.log(e));
