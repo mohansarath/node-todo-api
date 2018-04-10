4cf9d52822fbd30de884a5d9aa71384b02bb0b44
sarath <sarath.sct@gmail.com>
2018-04-10 17:17:05 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,38 +1,55 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data= {
-    id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
 
-var decoded=jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPAssword = '$2a$10$n4VzpkUZoHEv7p71OhlztuCaWbIhzr5xXFRtlS2/pxGG4vWACrnIy';
+bcrypt.compare(password, hashedPAssword, (err, res) => {
+    console.log(res);
+})
 
-// var message = 'I am user 1';
-// var hash = SHA256(message).toString();
 
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
 
-// var data = {
-//     id: 4
+
+// var data= {
+//     id: 10
 // };
 
-// var token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// }
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-// // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+// var decoded=jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// // var message = 'I am user 1';
+// // var hash = SHA256(message).toString();
 
-// if(resultHash === token.hash){
-//     console.log('Data not changed');
-// }else {
-//     console.log('Data changed. Dont trust');
-// }
+// // console.log(`Message: ${message}`);
+// // console.log(`Hash: ${hash}`);
+
+// // var data = {
+// //     id: 4
+// // };
+
+// // var token = {
+// //     data,
+// //     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// // }
+
+// // // token.data.id = 5;
+// // // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// // var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// // if(resultHash === token.hash){
+// //     console.log('Data not changed');
+// // }else {
+// //     console.log('Data changed. Dont trust');
+// // }
--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -61,9 +61,9 @@ UserSchema.statics.findByToken = function (token) {
     try {
         decoded = jwt.verify(token, '123abc')
     } catch (e) {
-    //     return new Promise((resolve, reject) => {
-    //         reject();
-    //     })
+        //     return new Promise((resolve, reject) => {
+        //         reject();
+        //     })
 
         return Promise.reject();
     }
@@ -75,6 +75,21 @@ UserSchema.statics.findByToken = function (token) {
     });
 
 };
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        });
+    } else {
+        next();
+    }
+});
 
 const User = mongoose.model('User', UserSchema);
 
